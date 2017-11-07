0b9cfa6110878eddb2f9140d4f0677b3be33c9ba
ciara <ciarapower88@gmail.com>
2017-11-07 02:42:40 +0000

    Minor comment modifications

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class Homepage extends React.Component{   //homepage which is the default index 
             <div >
             <img className=" center-block img-rounded img-responsive" src=".././img/header_AllBooks.jpg" alt="Books Homepage"/>
              <h1 className="BlackPageTitle">Homepage</h1>
-             <div className="homeMessage">   // message to display on homepage
+             <div className="homeMessage">   {/* message to display on homepage */}
              <p> <span style={{fontSize:'20px',fontWeight:'bold'}}> Welcome to BookShelf - the most useful book review website to be found ! </span><br/><br/> 
              Use the navigation bar above to navigate through sections.<br/>
              Remember - clicking the BookShelf logo on the left will take you back to this page!
